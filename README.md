5b2bf0710c8ea6f70174e347ce08f5a696fffc57
shahzad salamat <shahzad.salamat@yahoo.com>
2021-01-06 16:34:27 +0200

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-React Redux Ecommerce
-
-Introduction: In this project user can do online shopping.
-
-LIVE DEMO: https://shahzadsalamat.github.io/react-redux-ecommerce/
-
-Techonolgies: HTML, CSS  React: react redux, react hooks.
---
+++ precache-manifest.1539586448d001cc8af20f271769ac5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cfc97ebc1e86c2fe8e8a275932ed2a",
+    "url": "/react-redux-ecommerce/index.html"
+  },
+  {
+    "revision": "3dcc7a147753f54d6dd7",
+    "url": "/react-redux-ecommerce/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e256f83918c788852ba9",
+    "url": "/react-redux-ecommerce/static/css/main.412b54c9.chunk.css"
+  },
+  {
+    "revision": "3dcc7a147753f54d6dd7",
+    "url": "/react-redux-ecommerce/static/js/2.3be76a18.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-ecommerce/static/js/2.3be76a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e256f83918c788852ba9",
+    "url": "/react-redux-ecommerce/static/js/main.43b6f68a.chunk.js"
+  },
+  {
+    "revision": "a2abf4a7ca44f84c283a",
+    "url": "/react-redux-ecommerce/static/js/runtime-main.48a350dc.js"
+  }
+]);
